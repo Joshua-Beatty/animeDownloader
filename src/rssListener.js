2919a767069ade8694c5d1ebed94d69e1f851fc9
Joshua Beatty <joshuatb6@gmail.com>
2022-05-01 20:58:56 -0600

    changing to spawn

--- src/rssListener.js
+++ src/rssListener.js
@@ -1,44 +1,52 @@
-import { CronJob }  from 'cron';
+import { CronJob } from 'cron';
 import axios from 'axios';
 import fs from 'fs';
 import { XMLParser } from 'fast-xml-parser';
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
 const parser = new XMLParser();
 const job = new CronJob(
 	'0 0 * * * *',
 	main,
-    null,
-    false
+	null,
+	false
 );
-export {job, main};
+export { job, main };
 
 async function main() {
 	const config = JSON.parse(fs.readFileSync('src/config.json', 'utf8'));
 	const response = await axios.get("https://subsplease.org/rss/?r=1080")
 	const data = parser.parse(response.data);
 	let configUpdated = false;
-	for(const item of data.rss.channel.item){
-		for(const show of config.shows){
-			if(item.title.includes(show.titlePiece)){
-				if(show.last != (item.title)){
-					show.last = item.title;
-					configUpdated = true;
-					downloadTorrent(item.link)
+	for (const item of data.rss.channel.item) {
+		for (const show of config.shows) {
+			if (item.title.includes(show.titlePiece)) {
+				if (show.last != (item.title)) {
+					downloadTorrent(item.link, (success) => {
+						if (!success)
+							return;
+						show.last = item.title;
+						configUpdated = true;
+					})
+
 				}
 			}
 		}
 	}
-	if(configUpdated){
+	if (configUpdated) {
 		fs.writeFileSync('src/config.json', JSON.stringify(config, null, 4));
 	}
 }
-function downloadTorrent(link){
-	exec(`transmission-remote -a '${link}' -n ${process.env.TRANSMISSION_N_ARG}`, (error, stdout, stderr) => {
-		if (error) {
-		  console.error(`exec error: ${error}`);
-		  return;
-		}
-		console.log(`stdout: ${stdout}`);
-		console.error(`stderr: ${stderr}`);
-	  });
+function downloadTorrent(link, callback) {
+	const tr = spawn(`transmission-remote`, ['-a', link, '-n', process.env.TRANSMISSION_N_ARG])
+	tr.stdout.on('data', (data) => {
+		console.log(`stdout: ${data.replace(process.env.TRANSMISSION_N_ARG, '*'.repeat(process.env.TRANSMISSION_N_ARG.length))}`);
+	});
+
+	tr.stderr.on('data', (data) => {
+		console.log(`stderr: ${data.replace(process.env.TRANSMISSION_N_ARG, '*'.repeat(process.env.TRANSMISSION_N_ARG.length))}`);
+	});
+
+	tr.on('close', (code) => {
+		callback(code == 0)
+	});
 }
